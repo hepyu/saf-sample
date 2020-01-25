e51813113b985e7e592cc10c814a1ebc4b4d6c5e
hepengyuan <hpy253215039@163.com>
2020-01-25 18:39:47 +0800

    update

--- saf-sample-dubbo/saf-sample-dubbo-apollo/saf-sample-dubbo-apollo-web/kubernetes/deploy.sh
+++ saf-sample-dubbo/saf-sample-dubbo-apollo/saf-sample-dubbo-apollo-web/kubernetes/deploy.sh
@@ -1,9 +1,9 @@
-mkdir -p /datavip/k8s-data/saf-sample-allinone-web
+mkdir -p /datavip/k8s-data/saf-sample-dubbo-apollo-web
 
-kubectl apply -f saf-sample-allinone-web-prod-namespace.yaml
-kubectl apply -f saf-sample-allinone-web-prod-pv-local.yaml
-kubectl apply -f saf-sample-allinone-web-prod-pvc.yaml
-kubectl apply -f saf-sample-allinone-web-prod-configmap.yaml
-kubectl apply -f saf-sample-allinone-web-prod-deployment.yaml
-kubectl apply -f saf-sample-allinone-web-prod-service.yaml
-kubectl apply -f saf-sample-allinone-web-prod-ingress.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-namespace.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-pv-local.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-pvc.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-configmap.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-deployment.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-service.yaml
+kubectl apply -f saf-sample-dubbo-apollo-web-prod-ingress.yaml
