ba5706be6c76d7f5ad051140a09e7bccfa66608c
hpy <hpy253215039@163.com>
2020-02-13 18:30:44 +0800

    update

--- saf-sample-http/saf-sample-http-apache-httpcomponents/saf-sample-http-apache-httpcomponents-web-receive/src/main/java/com/future/saf/sample/http/apache/httpcomponents/web/receive/SafSampleHttpApplication.java
+++ saf-sample-http/saf-sample-http-apache-httpcomponents/saf-sample-http-apache-httpcomponents-web-receive/src/main/java/com/future/saf/sample/http/apache/httpcomponents/web/receive/SafSampleHttpApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.future.saf.configcenter.apollo.EnableApolloConfigAutoChangePrint;
+import com.future.saf.flowcontrol.sentinel.basic.EnableSentinel;
 
 @SpringBootApplication
 
 // 开启apollo配置中心
-@EnableApolloConfig(value = { "application", "saf.log.level" })
+@EnableApolloConfig(value = { "application", "saf.log.level", "sentinel.demo-web-receive" })
 // 系统启动时会打印@EnableApolloConfig中指定的namespace的初始化值；并且如果运行时会打印发生变化的配置。
 @EnableApolloConfigAutoChangePrint
-
+@EnableSentinel
 public class SafSampleHttpApplication {
 
 	public static void main(String[] args) {
--- saf-sample-http/saf-sample-http-apache-httpcomponents/saf-sample-http-apache-httpcomponents-web-send/src/main/java/com/future/saf/sample/http/apache/httpcomponents/web/send/SafSampleHttpApplication.java
+++ saf-sample-http/saf-sample-http-apache-httpcomponents/saf-sample-http-apache-httpcomponents-web-send/src/main/java/com/future/saf/sample/http/apache/httpcomponents/web/send/SafSampleHttpApplication.java
@@ -4,17 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.future.saf.configcenter.apollo.EnableApolloConfigAutoChangePrint;
+import com.future.saf.flowcontrol.sentinel.basic.EnableSentinel;
 import com.future.saf.http.apache.httpcomponents.syncimpl.EnableApacheHttpcomponentsBioClient;
 
 /**
  * Created by hepengyuan on 2018/09/27.
  */
 @SpringBootApplication
-@EnableApolloConfig(value = { "application", "saf.log.level" })
+@EnableApolloConfig(value = { "application", "saf.log.level", "sentinel.demo-web-send" })
 @EnableApolloConfigAutoChangePrint
 
 @EnableApacheHttpcomponentsBioClient(beanNamePrefix = "internet", instance = "internet", project = "demo-web-send")
 @EnableApacheHttpcomponentsBioClient(beanNamePrefix = "demoWebReceive", instance = "demo-web-receive", project = "demo-web-send")
+
+@EnableSentinel
 public class SafSampleHttpApplication {
 
 	public static void main(String[] args) {
