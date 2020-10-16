5e35452d8908cbefcbaf58296cfd67b12e174063
贺鹏远 <hepengyuan@hepengyuandeMacBook-Pro.local>
2020-10-16 15:09:26 +0800

    update

--- saf-sample-rocketmq/saf-sample-rocketmq-transaction/src/main/java/com/future/saf/sample/rocketmq/transaction/mq/config/RocketmqConfig.java
+++ saf-sample-rocketmq/saf-sample-rocketmq-transaction/src/main/java/com/future/saf/sample/rocketmq/transaction/mq/config/RocketmqConfig.java
@@ -2,12 +2,11 @@ package com.future.saf.sample.rocketmq.transaction.mq.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import lombok.Getter;
 
 @Configuration
-@PropertySource("classpath:application.yml") // 读取application.yml文件
+//@PropertySource("classpath:application.yml") // 读取application.yml文件
 @Getter
 public class RocketmqConfig {
 
@@ -19,4 +18,5 @@ public class RocketmqConfig {
 
 	@Value("${rocketmq.producer.send-msg-timeout}")
 	private int sendMsgTimeout;
+
 }
